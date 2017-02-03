298d6bf2ceafb6d73aa72a9f3407457230887b1a
Yukine-Alterma <saphira1904@hotmail.com>
2017-02-03 14:46:30 -0500

    Random Game 0.2
    Updated Random Game with the ability to choose to play again.
    Added a better way to tally up the score.

--- oddeven.py
+++ oddeven.py
@@ -1,65 +1,91 @@
 import random
 
-num = str(input("Enter below or above 50 : "))
-color = str(input("Enter either red or black : "))
-eoro = str(input("Enter either even or odd : "))
 
-minrange = random.randint(1,50)
-maxrange = random.randint(50,100)
+def mainprog():
+    correct = 0
 
-prognum = random.randint(1,100)
-progsel = random.randint(1,2)
+#Player choosing
+    num = str(input("Enter below or above 50 : "))
+    color = str(input("Enter either red or black : "))
+    eoro = str(input("Enter either even or odd : "))
 
-if (int(prognum) >= 50):
-    progset = "above"
-elif (int(prognum) < 50):
-    progset = "below"
+    minrange = random.randint(1,50)
+    maxrange = random.randint(50,100)
 
-if (progsel == 1):
-    progcolor = "black"
-elif (progsel == 2):
-    progcolor = "red"
+    prognum = random.randint(1,100)
+    progsel = random.randint(1,2)
 
-mod = (prognum % 2)
-print(mod)
-if mod == 0:
-    print("The number is even.")
-    progeoro = "even"
-elif mod != 0:
-    print("The number is odd.")
-    progeoro = "odd"
+#AI choosing
+    if (int(prognum) >= 50):
+        progset = "above"
+    elif (int(prognum) < 50):
+        progset = "below"
 
-print(progsel)            
-print(str(progcolor))
-print(minrange)
-print(maxrange)
-print(prognum)
+    if (progsel == 1):
+        progcolor = "black"
+    elif (progsel == 2):
+        progcolor = "red"
 
-print("Your answers : ")
-print("Above or Below : "+num)
-print("Red or Black : "+color)
-print("Even or Odd : "+eoro)
-print("\n")
+    mod = (prognum % 2)
+    print(mod)
+    if mod == 0:
+        print("The number is even.")
+        progeoro = "even"
+    elif mod != 0:
+        print("The number is odd.")
+        progeoro = "odd"
 
-print("Computer's Answers : ")
-print("Above or Below : "+progset)
-print("Red or Black : "+progcolor)
-print("Even or Odd : "+progeoro)
-print("\n")
+#print(progsel)            
+#print(str(progcolor))
+#print(minrange)
+#print(maxrange)
+#print(prognum)
 
-if (str(num) == str(progset)):
-    print("Placement : You were within the parameters.")
+
+# Checks to see where the selections sit
+    if (str(num) == str(progset)):
+        print("Placement : You were within the parameters.")
+        correct+=1
+    else:
+        print("Placement : You were not within the parameters.")
+
     if (str(color) == str(progcolor)):
         print("Color : Your choice was correct.")
-        if (str(eoro) == str(progeoro)):
-            print("Even or Odd : Your choice was correct.")
-            print("Result : 3/3")
-        else:
-            print("Even or Odd : Your choice was incorrect.")
-            print("Result : 2/3")
+        correct+=1
     else:
         print("Color : Your choice was incorrect.")
-        print("Result : 1/3")
-else:
-    print("Placement : You were not within the parameters.")
-    print("Result : 0/3")
+
+    if (str(eoro) == str(progeoro)):
+        print("Even or Odd : Your choice was correct.")
+        correct+=1
+    else:
+        print("Even or Odd : Your choice was incorrect.")
+
+    print("Your answers : ")
+    print("Above or Below : "+num)
+    print("Red or Black : "+color)
+    print("Even or Odd : "+eoro)
+    print("\n")
+
+    print("Computer's Answers : ")
+    print("Above or Below : "+progset)
+    print("Red or Black : "+progcolor)
+    print("Even or Odd : "+progeoro)
+    print("\n")
+
+    print("Your results : "+str(correct)+"/3")
+
+    restart()
+
+def restart():
+    restart = input("Would you like to restart this program?")
+    if restart == "yes" or restart == "y":
+        mainprog()
+    if restart == "n" or restart == "no":
+        print ("Terminating Script...")
+        print ("Thanks for playing!")
+
+def main():
+    window.mainloop()
+
+mainprog()
